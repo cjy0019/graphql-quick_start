22b3eda916e4d6f46010f67017954ff26645e000
JYCHO <cjy0029@naver.com>
2022-02-04 00:20:40 +0900

    feat : deleteMovie gql 구현

--- db/db.js
+++ db/db.js
@@ -36,11 +36,11 @@
 //   return filteredPeople[0];
 // };
 
-export const movies = [
+export let movies = [
   {
     id: 0,
     name: 'Thor - Thunder man',
-    score: 0.9,
+    score: 11,
   },
   {
     id: 1,
@@ -74,4 +74,14 @@ export const deleteMovie = (id) => {
   } else {
     return false;
   }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,18 @@
-import { getById, people } from '../db/db';
+import { getById, getMovies, addMovie, deleteMovie } from '../db/db';
+// import { getById, people } from '../db/db';
 
 const resolvers = {
+  // Query: {
+  //   people: () => people,
+  //   person: (_, args) => getById(args.id),
+  // },
   Query: {
-    people: () => people,
-    person: (_, args) => getById(args.id),
+    movies: () => getMovies(),
+    movie: (_, args) => getById(args.id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
