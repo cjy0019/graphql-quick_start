0debd539911cf20f1879687414cd2a481a2708dc
JYCHO <cjy0029@naver.com>
2022-02-03 23:58:28 +0900

    feat : movie api 설계

--- db/db.js
+++ db/db.js
@@ -1,37 +1,77 @@
-export const people = [
+// export const people = [
+//   {
+//     id: 0,
+//     name: 'Leo Messi',
+//     age: 34,
+//     gender: 'male',
+//   },
+//   {
+//     id: 1,
+//     name: 'Cristiano Ronaldo',
+//     age: 34,
+//     gender: 'male',
+//   },
+//   {
+//     id: 2,
+//     name: 'Son HeungMin',
+//     age: 31,
+//     gender: 'male',
+//   },
+//   {
+//     id: 3,
+//     name: 'Park JiSung',
+//     age: 34,
+//     gender: 'male',
+//   },
+//   {
+//     id: 4,
+//     name: 'Joe Cole',
+//     age: 40,
+//     gender: 'male',
+//   },
+// ];
+
+// export const getById = (id) => {
+//   const filteredPeople = people.filter((person) => id === person.id);
+//   return filteredPeople[0];
+// };
+
+export const movies = [
   {
-    id: '0',
-    name: 'Leo Messi',
-    age: 34,
-    gender: 'male',
+    id: 0,
+    name: 'Thor - Thunder man',
+    score: 0.9,
   },
   {
-    id: '1',
-    name: 'Cristiano Ronaldo',
-    age: 34,
-    gender: 'male',
+    id: 1,
+    name: 'SpiderMan - No Way Home',
+    score: 9,
   },
   {
-    id: '2',
-    name: 'Son HeungMin',
-    age: 31,
-    gender: 'male',
+    id: 2,
+    name: 'The Avengers - End Game',
+    score: 99,
   },
   {
-    id: '3',
-    name: 'Park JiSung',
-    age: 34,
-    gender: 'male',
-  },
-  {
-    id: '4',
-    name: 'Joe Cole',
-    age: 40,
-    gender: 'male',
+    id: 3,
+    name: 'Black Panther',
+    score: 3,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => id === people.id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,7 +3,7 @@ import { getById, people } from '../db/db';
 const resolvers = {
   Query: {
     people: () => people,
-    person: () => getById(),
+    person: (_, args) => getById(args.id),
   },
 };
 
